79c420becf72d30391066f646e0b0674ce53a1bb
Alfredo AD <alfredo7891@gmail.com>
2016-05-26 15:46:05 -0500

    update window.plugins.PushbotsPlugin.unregister

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -39,7 +39,7 @@ var app = {
         // Should be called once the device is registered successfully with Apple or Google servers
         window.plugins.PushbotsPlugin.on("registered", function(token){
             alert("Registro UNOp "+token);
-            window.plugins.PushbotsPlugin.unregister();
+            window.plugins.PushbotsPlugin.unregister("KsaFozne");
             window.plugins.PushbotsPlugin.updateAlias("KsaFozne");
         });
 
